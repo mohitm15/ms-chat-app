c9f010152d16dc591756de80ae2c80791efd06fd
Mohit Maroliya <mmaroliya@gmail.com>
2022-01-30 19:36:46 +0530

    Setting the env variables-2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-
 import "./App.css";
 import { ChatEngine } from "react-chat-engine";
 
-
 function App() {
-
   const project_id = process.env.REACT_APP_PROJECT_ID;
-  const user_name = process.env.REACT_APP_USER_NAME;
+  const user_name = process.env.REACT_APP_USERNAME;
   const user_secret = process.env.REACT_APP_USER_SECRET;
 
   return (
     <div className="container">
-<ChatEngine
-			height='100vh'
-			userName={user_name}
-			userSecret={user_secret}
-			projectID={project_id}
-		/>
+      <ChatEngine
+        height="100vh"
+        userName={user_name}
+        userSecret={user_secret}
+        projectID={project_id}
+      />
     </div>
   );
-} 
+}
 
 export default App;
