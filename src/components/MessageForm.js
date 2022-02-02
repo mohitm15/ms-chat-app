dd43419ea663dbbc1dc2c9687413a5a24e15dfa2
Mohit Maroliya <mmaroliya@gmail.com>
2022-02-02 19:36:43 +0530

    Adding the upload image functionality

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,32 +1,65 @@
-import React, { useState } from 'react';
-import { sendMessage, isTyping } from 'react-chat-engine';
+import React, { useState } from "react";
+import { sendMessage, isTyping } from "react-chat-engine";
 
 const MessageForm = (props) => {
+  const [value, setValue] = useState("");
 
-  const [value, setValue] = useState('');
-
-  const {chatId, creds} = props;
-
+  const { chatId, creds } = props;
 
   const handleSubmit = (e) => {
-      e.preventDefault(); //to not do a browser refresh
-      const text = value.trim();
+    e.preventDefault(); //to not do a browser refresh
+    const text = value.trim();
 
-      if(text.length > 0) sendMessage(creds, chatId, {text});
+    if (text.length > 0) sendMessage(creds, chatId, { text });
 
-      setValue('');
-  }
+    setValue("");
+  };
 
   const handleChange = (e) => {
-    setValue(e.target.value)
-    isTyping(props, chatId)
-}
+    setValue(e.target.value);
+    isTyping(props, chatId);
+  };
+
+  const handleUpload = (e) => {
+    sendMessage(creds, chatId, { files: e.target.files, text: "" });
+  };
 
   return (
-        <form className='form relative' onSubmit={handleSubmit}>
-            <input className='form-input' placeholder='Send a Message ...' value={value} onChange={handleChange} onSubmit={handleSubmit} />
-        </form>
-    );
+    <form
+      className="form relative flex flex-row justify-between items-center"
+      onSubmit={handleSubmit}
+    >
+      <div>
+        <input
+          className="form-input p-1 border-xs border-gray-600"
+          placeholder="Send a Message ..."
+          value={value}
+          onChange={handleChange}
+          onSubmit={handleSubmit}
+        />
+      </div>
+      <div>
+        <label htmlFor="upload_button">
+          <span className="bg-blue-400 px-2 py-2 rounded-lg ml-2 border-2 border-black hover:bg-blue-500">
+            <i class="fas fa-image"></i>
+          </span>
+        </label>
+        <input
+          type="file"
+          multiple={false}
+          id="upload_button"
+          className="hidden"
+          onChange={handleUpload}
+        />
+        <button
+          type="submit"
+          className="send_button bg-blue-400 px-2 py-1 rounded-lg ml-2 border-2 border-black hover:bg-blue-500"
+        >
+          <i class="fas fa-paper-plane"></i>
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default MessageForm;
