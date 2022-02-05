9c8074b77fee12527fb25a262542bffa73dab6db
Mohit Maroliya <mmaroliya@gmail.com>
2022-02-05 20:36:14 +0530

    Adding the Login Form for users

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import "./App.css";
 import { ChatEngine } from "react-chat-engine";
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 
-
+const project_id = "04903c43-e194-4310-a4e7-8c48fac23c8d"
 function App() {
-  const project_id = process.env.REACT_APP_PROJECT_ID;
-  const user_name = process.env.REACT_APP_USERNAME;
-  const user_secret = process.env.REACT_APP_USER_SECRET;
+  //const project_id = process.env.REACT_APP_PROJECT_ID;
+
+  if(!localStorage.getItem('username')) return <LoginForm />
 
   return (
     <div className="container">
       <ChatEngine
         height="100vh"
-        userName={user_name}
-        userSecret={user_secret}
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
         projectID={project_id}
         renderChatFeed={ (chatAppProps) => <ChatFeed {...chatAppProps} /> }
       />
