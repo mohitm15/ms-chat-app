8f70ecb7e81351a12dfae51ffc9585adba003f24
Mohit Maroliya <mmaroliya@gmail.com>
2022-02-11 20:23:04 +0530

    Commenting out the console logs

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -33,7 +33,7 @@ const ChatFeed = (props) => {
 
     return keys.map((key, index) => {
       const message = messages[key];
-      console.log(message);
+      //console.log(message);
       const lastMessageKey = index === 0 ? null : keys[index - 1];
       const isMyMessage = userName === message.sender.username;
 
